50ae12a153d4130362d30c68cb55002d3e9cd3aa
matfolio <tunde4mons@outlook.com>
2019-08-11 04:23:20 +0300

    Updates

---
+++ precache-manifest.133495b749f6465404994c6e3ffeb094.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45ee9f8b0bfaf65b0984eea6af8d675",
+    "url": "/react-page-app/index.html"
+  },
+  {
+    "revision": "8605caa485d0226f58f7",
+    "url": "/react-page-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f2db75055968442b1a9d",
+    "url": "/react-page-app/static/js/2.2f80ea3e.chunk.js"
+  },
+  {
+    "revision": "8605caa485d0226f58f7",
+    "url": "/react-page-app/static/js/main.732abce0.chunk.js"
+  },
+  {
+    "revision": "7c8a625d1ee75d2e13ea",
+    "url": "/react-page-app/static/js/runtime~main.4c0980af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-page-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
